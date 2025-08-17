96e1417f87295dcab187e96dbacbf450b1cd20e2
hannguyendd <han.nguyenex@gmail.com>
2025-08-17 15:02:22 +0700

    chore: add packages and configure services in Program.cs

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using ProductService.Infrastructure;
+using ProductService.Interfaces;
+using ProductService.Services;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+// Register services
+builder.Services.AddScoped<IAppProductService, AppProductService>();
+
 
 var app = builder.Build();
 
@@ -16,6 +28,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Map controllers
+app.MapControllers();
 
 app.Run();
 
